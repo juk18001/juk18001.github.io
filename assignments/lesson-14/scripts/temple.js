bb530ec1b4ee09370478803b3ce4f2f5c045f896
juke18001 <juk18001@byui.edu>
2019-04-12 17:23:32 -0500

    initial load of final

---
+++ assignments/lesson-14/scripts/temple.js
@@ -0,0 +1,37 @@
+//get the json data
+var requestURL = 'https://juk18002.github.io/assignments/lesson-14/data/temples.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function () {
+    var varJSON = request.response;
+
+    updateTemples(varJSON);
+}
+
+function updateTemples(textJSON) {
+    var templesJson = textJSON.temples;
+
+    for (var i = 0; i < templesJson.length; i++) {
+        var objJSON = templesJson[i];
+        if (objJSON.city == 'Frankfurt') {
+            var ul = document.getElementById('fc_ul')
+        } else if (objJSON.city == 'London') {
+            var ul = document.getElementById('londonClosures')
+        } else if (objJSON.city == 'Washington') {
+            var ul = document.getElementById('dcClosures')
+        } else if (objJSON.city == 'Rome') {
+            var ul = document.getElementById('romeClosures')
+        }
+
+        var closureObj = objJSON.ordinanceSchedule.baptismal;
+        for (j = 0; j < closureObj.length; j++) {
+            var closureLi = document.createElement('li');
+            closureLi.innerHTML = closureObj[j];
+            ul.appendChild(closureLi);
+        }
+    }
+}
